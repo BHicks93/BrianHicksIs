d09678dfecfa0598346d8f8597741e351d43049e
BRIAN HICKS <bhicks@risd.edu>
2014-10-30 12:19:03 -0400

    checking

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,10 +1,10 @@
-$(":checkbox").change(function() {
+$(":checkbox").change(function() { console.log("checked1");
     if(this.checked) {
         $( "#Posters" ).show( "fast" );
-        console.log("checked")
+        console.log("checked2");
     }
 });
 
-
+console.log("checked3");
 //if boxes have been checked then show a certain div.
 //define all divs with images and the in css desplay:none;

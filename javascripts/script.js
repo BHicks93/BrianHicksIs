1dfa1f0d2f80bcc345643fe5138b57972b321411
BRIAN HICKS <bhicks@risd.edu>
2014-10-29 23:23:19 -0400

    Add query and javascript

---
+++ javascripts/script.js
@@ -0,0 +1,9 @@
+$( ".cbgroup1:checked" "label" ).show(function() {
+  $( "content2" )
+  });
+});
+ 
+
+
+//if boxes have been checked then show a certain div.
+//define all divs with images and the in css desplay:none;

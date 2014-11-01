bcf9933bf83f23fb03b46b2d7a932f282684a699
BRIAN HICKS <bhicks@risd.edu>
2014-11-01 11:54:20 -0400

    added slide show .js

---
+++ javascripts/slide_show.js
@@ -0,0 +1,17 @@
+var image1=new Image()
+image1.src="boxingring.png"
+var image2=new Image()
+image2.src="creativity.jpg"
+var image3=new Image()
+image3.src="gears.png"
+
+var step=1
+function slideit(){
+document.images.slide.src=eval("image"+step+".src")
+if(step<3)
+step++
+else
+step=1
+setTimeout("slideit()",2500)
+}
+slideit()

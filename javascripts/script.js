a1fd5e0673876e3b6a3ff260965ccacfc46ce53c
BRIAN HICKS <bhicks@risd.edu>
2014-11-02 09:45:07 -0500

    unchecked working
    added an else { } so that when the check boxes are unchecked the
    content goes away and graphic design and stuff comes back

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,30 +1,11 @@
-/*
-//Working on See All feature
-
-$("#cbgroup1_master").change(function() {console.log("checked1");
-
-// define var
-var master = $ (this)
-var cn = "cbgroup1"
-
-//call function, passing var
-
-	var cbarray = document.getElementsByClassName(cn);
-	for(var i = 0; i < cbarray.length; i++) { 
-		var cb = document.getElementById(cbarray[i].id); console.log(cbarray[i].id);
-		cb.checked = master.checked;
-			}})
-			
-
-//	window.addEventListener("scroll", yScroll);
-*/
-
-
 //When checkbox is checked turn on information (posters, type, etc.) and turn off 'content'
 $(".checkboxes #cb1_0").change(function() { 
     if(this.checked) {
         $( "#About" ).show( "fast" );
         $( ".content" ).hide( "fast" );
+  } else {
+    $( "#About" ).hide( "fast" );
+    $( ".content" ).show( "fast" );
     }
 });
 
@@ -32,6 +13,9 @@ $(".checkboxes #cb1_1").change(function() {
     if(this.checked) {
         $( "#Posters" ).show( "fast" );
         $( ".content" ).hide( "fast" );
+    } else {
+    $( "#Posters" ).hide( "fast" );
+    $( ".content" ).show( "fast" );
     }
 });
 
@@ -39,6 +23,9 @@ $(".checkboxes #cb1_2").change(function() {
     if(this.checked) {
         $( "#Typography" ).show( "fast" );
         $( ".content" ).hide( "fast" );
+    } else {
+    $( "#Typography" ).hide( "fast" );
+    $( ".content" ).show( "fast" );
     }
 });
 
@@ -46,6 +33,9 @@ $(".checkboxes #cb1_3").change(function() {
     if(this.checked) {
         $( "#Identity" ).show( "fast" );
         $( ".content" ).hide( "fast" );
+    } else {
+    $( "#Identity" ).hide( "fast" );
+    $( ".content" ).show( "fast" );
     }
 });
 
@@ -53,6 +43,9 @@ $(".checkboxes #cb1_4").change(function() {
     if(this.checked) {
         $( "#Motion" ).show( "fast" );
         $( ".content" ).hide( "fast" );
+    } else {
+    $( "#Motion" ).hide( "fast" );
+    $( ".content" ).show( "fast" );
     }
 });
 
@@ -60,31 +53,9 @@ $(".checkboxes #cb1_5").change(function() {
     if(this.checked) {
         $( "#Writing" ).show( "fast" );
         $( ".content" ).hide( "fast" );
+    } else {
+    $( "#Writing" ).hide( "fast" );
+    $( ".content" ).show( "fast" );
     }
 });
 
-$(".checkboxes #cb1_6").change(function() { 
-    if(this.checked) {
-        location.reload();
-    }
-});
-
-$(function(){
-    $(window).bind("load resize", function(){
-        _winHeight = $(window).height();
-
-        // Setting Height
-        $('.writing').css({'height':_winHeight * 0.9}); // 0.5 = 50%, 0.8 = 80%
-    });
-});
-
-
-//if boxes have been checked then show a certain div.
-//define all divs with images and the in css desplay:none;
-//".checkboes input"
-
-
-//if its in the list then draw that section
-
-//	$('checkbox')
-//	togglecheckboxes(this,'cbgroup1')"

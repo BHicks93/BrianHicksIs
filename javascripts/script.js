d4dfa1000425d7d6e027c1201b30a4728d600788
BRIAN HICKS <bhicks@risd.edu>
2014-11-02 19:14:02 -0500

    hide all other content
    when checked it hides all other content. CSS changes: when checked
    nothing will show that its checked

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,7 +2,13 @@
 $(".checkboxes #cb1_0").change(function() { 
     if(this.checked) {
         $( "#About" ).show( "fast" );
+        
         $( ".content" ).hide( "fast" );
+        $( "#Posters" ).hide( "fast" );
+        $( "#Typography" ).hide( "fast" );
+        $( "#Identity" ).hide( "fast" );
+        $( "#Motion" ).hide( "fast" );
+        $( "#Writing" ).hide( "fast" );
   } else {
     $( "#About" ).hide( "fast" );
     }
@@ -11,7 +17,13 @@ $(".checkboxes #cb1_0").change(function() {
 $(".checkboxes #cb1_1").change(function() { 
     if(this.checked) {
         $( "#Posters" ).show( "fast" );
+        
         $( ".content" ).hide( "fast" );
+        $( "#About" ).hide( "fast" );
+        $( "#Typography" ).hide( "fast" );
+        $( "#Identity" ).hide( "fast" );
+        $( "#Motion" ).hide( "fast" );
+        $( "#Writing" ).hide( "fast" );
     } else {
     $( "#Posters" ).hide( "fast" );
     }
@@ -21,6 +33,11 @@ $(".checkboxes #cb1_2").change(function() {
     if(this.checked) {
         $( "#Typography" ).show( "fast" );
         $( ".content" ).hide( "fast" );
+        $( "#About" ).hide( "fast" );
+        $( "#Posters" ).hide( "fast" );
+        $( "#Identity" ).hide( "fast" );
+        $( "#Motion" ).hide( "fast" );
+        $( "#Writing" ).hide( "fast" );
     } else {
     $( "#Typography" ).hide( "fast" );
     }
@@ -29,7 +46,13 @@ $(".checkboxes #cb1_2").change(function() {
 $(".checkboxes #cb1_3").change(function() { 
     if(this.checked) {
         $( "#Identity" ).show( "fast" );
+        
         $( ".content" ).hide( "fast" );
+        $( "#About" ).hide( "fast" );
+        $( "#Posters" ).hide( "fast" );
+        $( "#Typography" ).hide( "fast" );
+        $( "#Motion" ).hide( "fast" );
+        $( "#Writing" ).hide( "fast" );
     } else {
     $( "#Identity" ).hide( "fast" );
     }
@@ -38,7 +61,13 @@ $(".checkboxes #cb1_3").change(function() {
 $(".checkboxes #cb1_4").change(function() { 
     if(this.checked) {
         $( "#Motion" ).show( "fast" );
+        
         $( ".content" ).hide( "fast" );
+        $( "#About" ).hide( "fast" );
+        $( "#Posters" ).hide( "fast" );
+        $( "#Typography" ).hide( "fast" );
+        $( "#Identity" ).hide( "fast" );
+        $( "#Writing" ).hide( "fast" );
     } else {
     $( "#Motion" ).hide( "fast" );
     }
@@ -47,7 +76,13 @@ $(".checkboxes #cb1_4").change(function() {
 $(".checkboxes #cb1_5").change(function() { 
     if(this.checked) {
         $( "#Writing" ).show( "fast" );
+        
         $( ".content" ).hide( "fast" );
+        $( "#About" ).hide( "fast" );
+        $( "#Posters" ).hide( "fast" );
+        $( "#Typography" ).hide( "fast" );
+        $( "#Identity" ).hide( "fast" );
+        $( "#Motion" ).hide( "fast" );
     } else {
     $( "#Writing" ).hide( "fast" );
     }
